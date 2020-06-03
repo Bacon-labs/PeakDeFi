8c010efa2872a5cac08df3cfab6e5b27b6a135ed
guillaume palayer <guillaume.palayer@gmail.com>
2020-06-03 18:10:24 +0200

    created landing page layout and onboarding

--- src/assets/js/dashkit.js
+++ src/assets/js/dashkit.js
@@ -542,3 +542,20 @@
     });
   }
 })();
+
+
+autoPlayYouTubeModal();
+
+//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+function autoPlayYouTubeModal() {
+    var trigger = $("body").find('[data-target="#video"]');
+    trigger.click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-theVideo"),
+            videoSRCauto = videoSRC + "?autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+}
